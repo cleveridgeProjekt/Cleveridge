b5453ca62c5a572b640e6278142e0c5794695ea5
anastasiia-skatova <anastasiia.skatova@adelt.it>
2025-07-23 10:38:34 +0200

    Font awesome

--- resources/js/components/layout/Sidebar.vue
+++ resources/js/components/layout/Sidebar.vue
@@ -16,17 +16,17 @@ const toggleSubmenu = () => {
 
 <template>
     <div id="sidebar" class="sidebar">
-        <RouterLink class="nav-item" to="/"><i class="fal fa-home"></i> Dashboard</RouterLink>
-        <RouterLink class="nav-item" to="/fridge"><i class="fal fa-ice-cream"></i> What's in your fridge</RouterLink>
-        <RouterLink class="nav-item" to="/products"><i class="fal fa-apple-alt"></i> Produkte</RouterLink>
-        <RouterLink class="nav-item" to="/shopping-list"><i class="fal fa-list-alt"></i> Einkaufsliste</RouterLink>
+        <RouterLink class="nav-item" to="/"><i class="fas fa-home"></i> Dashboard</RouterLink>
+        <RouterLink class="nav-item" to="/fridge"><i class="fas fa-ice-cream"></i> What's in your fridge</RouterLink>
+        <RouterLink class="nav-item" to="/products"><i class="fas fa-apple-alt"></i> Produkte</RouterLink>
+        <RouterLink class="nav-item" to="/shopping-list"><i class="fas fa-list-alt"></i> Einkaufsliste</RouterLink>
         <div class="submenu-toggle" @click="toggleSubmenu">
-            <span class="submenu-link"><i class="fal fa-layer-group"></i> Finanzen <i :class="showSubmenu ? 'fal fa-angle-up' : 'fal fa-angle-down'"></i></span>
+            <span class="submenu-link"><i class="fas fa-layer-group"></i> Finanzen <i :class="showSubmenu ? 'fas fa-angle-up' : 'fal fa-angle-down'"></i></span>
         </div>
         <div v-if="showSubmenu" class="submenu">
-            <RouterLink class="submenu-item" to="/status"><i class="fal fa-lightbulb"></i> Cleveridge Status</RouterLink>
-            <RouterLink class="submenu-item" to="/expiry"><i class="fal fa-exclamation-triangle"></i> Ablaufwarnungen</RouterLink>
-            <RouterLink class="submenu-item" to="/barcode"><i class="fal fa-barcode"></i> Barcode Scannen!</RouterLink>
+            <RouterLink class="submenu-item" to="/status"><i class="fas fa-lightbulb"></i> Cleveridge Status</RouterLink>
+            <RouterLink class="submenu-item" to="/expiry"><i class="fas fa-exclamation-triangle"></i> Ablaufwarnungen</RouterLink>
+            <RouterLink class="submenu-item" to="/barcode"><i class="fas fa-barcode"></i> Barcode Scannen!</RouterLink>
         </div>
     </div>
 </template>
@@ -38,7 +38,7 @@ const toggleSubmenu = () => {
     padding-top: 28px;
     width: 300px;
     height: calc(100vh - 62px);
-    background-color: #0067b8;
+    background-color: #0c5288;
     color: white;
     display: grid;
     grid-auto-rows: max-content;
