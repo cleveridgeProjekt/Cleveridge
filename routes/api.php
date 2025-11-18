be5e89ffddc03ba89a3dc7ba0a42fdeb8d2da261
laurianabocanegra <laurita200314@gmail.com>
2025-11-18 16:24:12 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SensorController;
+use App\Http\Controllers\CameraController;
 
 Route::post('/sensor', [SensorController::class, 'store']);
 Route::get('/status', [SensorController::class, 'latest']);
+
+Route::get('/camera/check', [CameraController::class, 'checkCommand']);
+Route::post('/camera/upload', [CameraController::class, 'uploadPhotoAndResults']);
