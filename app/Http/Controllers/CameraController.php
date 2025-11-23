a038827591478d8ffe59cd6cce2636c5fdd3f969
laurianabocanegra <laurita200314@gmail.com>
2025-11-23 13:38:21 +0100

    Update CameraController.php
    integration google vision

--- app/Http/Controllers/CameraController.php
+++ app/Http/Controllers/CameraController.php
@@ -8,30 +8,26 @@ use Illuminate\Support\Facades\Storage;
 
 class CameraController extends Controller
 {
-
+    // 1. TRIGGER (Web Button)
     public function triggerCommand()
     {
-
         Cache::put('camera_trigger', true, 60);
-        
-        return response()->json(['status' => 'Orden enviada a la Pi']);
+        return response()->json(['status' => 'Command sent to Pi']);
     }
 
-
+    // 2. CHECK (Raspberry Pi)
     public function checkCommand()
     {
-
         $shouldSnap = Cache::get('camera_trigger', false);
 
         if ($shouldSnap) {
-
             Cache::forget('camera_trigger');
         }
 
         return response()->json(['snap' => $shouldSnap]);
     }
 
-
+    // 3. UPLOAD (Raspberry Pi)
     public function uploadPhotoAndResults(Request $request)
     {
         if (!$request->hasFile('file')) {
@@ -39,24 +35,26 @@ class CameraController extends Controller
         }
 
         $path = $request->file('file')->store('uploads', 'public');
+
+        $filename = basename($path);
         
+
+        $forcedUrl = '/storage/uploads/' . $filename;
 
         $products = json_decode($request->input('products', '[]'));
 
-
         $data = [
-            'image_url' => '/storage/' . $path, // Ruta web de la imagen
+            'image_url' => $forcedUrl, 
             'products' => $products,
             'timestamp' => now()->toDateTimeString()
         ];
         
-
         Cache::put('latest_camera_data', $data);
 
         return response()->json(['status' => 'success', 'path' => $path]);
     }
 
-
+    // 4. GET DATA (Vue Frontend)
     public function getLatestData()
     {
         $data = Cache::get('latest_camera_data', [
